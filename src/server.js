3af07673da678cb420789f20ebb198b308f1fddf
Elias <elias.souza.es609@gmail.com>
2023-07-21 15:54:25 -0300

    upgrade on server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
+// Importa o aplicativo do servidor express (app) definido no arquivo app.js
 const app = require('./app');
+// Importa o pacote dotenv para carregar variáveis de ambiente a partir de um arquivo .env
 require('dotenv').config();
+// Obtém a porta do servidor a partir das variáveis de ambiente
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log(`Servidor ligado na porta: ${PORT}`))
-
-
+// Inicia o servidor express para escutar na porta especificada
+app.listen(PORT, () => console.log(`Servidor ligado na porta: ${PORT}`));
