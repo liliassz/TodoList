e4e31dc0ba33a80edc66f7a917df6b5aa2013eb6
Elias <elias.souza.es609@gmail.com>
2023-07-21 15:39:31 -0300

    Update FrontEnd

--- public/js/script.js
+++ public/js/script.js
@@ -1,143 +1,75 @@
-const tbody = document.querySelector('tbody')
-const addForm = document.querySelector('.add-form')
-const inputTask = document.querySelector('.input-task')
+// Importar o pacote dotenv para carregar variáveis de ambiente do arquivo .env
+require('dotenv').config();
 
+// Definir a variável PORT obtendo seu valor do arquivo .env
+const PORT = process.env.PORT;
+
+// Obter referências para elementos HTML usando seletores
+const tbody = document.querySelector('tbody');
+const addForm = document.querySelector('.add-form');
+const inputTask = document.querySelector('.input-task');
+
+// Função assíncrona para buscar as tarefas da API usando fetch
 const fetchTasks = async () => {
-   const response = await fetch('http://localhost:3333/tasks');
-   const tasks = await response.json();
-   return tasks;
+    try {
+        const response = await fetch(`http://localhost:${PORT}/tasks`);
+        const tasks = await response.json();
+        return tasks;
+    } catch (error) {
+        console.error('Erro ao buscar as tarefas:', error);
+        return [];
+    }
 };
 
+// Função assíncrona para adicionar uma nova tarefa à API usando fetch
 const addTask = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const task = { title: inputTask.value
-    };
+    const task = { title: inputTask.value };
 
-    await fetch('http://localhost:3333/tasks', {
-        method: 'post',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify(task),
-    });
+    try {
+        await fetch(`http://localhost:${PORT}/tasks`, {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(task),
+        });
 
-    loadTasks();
-    inputTask.value = ''
+        // Após adicionar a tarefa, recarregar a lista de tarefas e limpar o campo de entrada
+        loadTasks();
+        inputTask.value = '';
+    } catch (error) {
+        console.error('Erro ao adicionar a tarefa:', error);
+    }
 };
 
+// Função assíncrona para excluir uma tarefa da API usando fetch
 const deleteTask = async (id) => {
-    await fetch(`http://localhost:3333/tasks/${id}`, {
-        method: 'delete'
-    });
-    loadTasks();
+    try {
+        await fetch(`http://localhost:${PORT}/tasks/${id}`, {
+            method: 'delete',
+        });
+
+        // Após excluir a tarefa, recarregar a lista de tarefas
+        loadTasks();
+    } catch (error) {
+        console.error('Erro ao excluir a tarefa:', error);
+    }
 };
 
+// Função assíncrona para atualizar uma tarefa da API usando fetch
 const updateTask = async (task) => {
     const { id, title, status } = task;
 
-    await fetch(`http://localhost:3333/tasks/${id}`, {
-        method: 'put',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title, status }),
-    });
+    try {
+        await fetch(`http://localhost:${PORT}/tasks/${id}`, {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title, status }),
+        });
 
-    loadTasks();
-};
-
-const formatDate = (dateUTC) => {
-    const options = { dateStyle: 'long', timeStyle: 'short'}
-    const date = new Date(dateUTC).toLocaleString('pt-br', options);
-    return date;
-}
-
-const createElement = (tag, innerText = '', innerHTML = '') => {
-    const element = document.createElement(tag);
-
-    if(innerText) {
-        element.innerText = innerText;
+        // Após atualizar a tarefa, recarregar a lista de tarefas
+        loadTasks();
+    } catch (error) {
+        console.error('Erro ao atualizar a tarefa:', error);
     }
-
-    if(innerHTML){
-        element.innerHTML = innerHTML;
-    }
-
-    return element;
 };
-
-const createSelect = (value) => {
-    const options = `
-    <option value="pendente">pendente</option>
-    <option value="em andamento">em andamento</option>
-    <option value="concluida">concluida</option>
-    `;
-
-    const select = createElement('select', '', options);
-
-    select.value = value;
-
-    return select
-};
-
-const createRow = (task) => {
-    const { id, title, created_at, status } = task;
-
-    const tr = createElement('tr');
-    const tdTitle = createElement('td', title);
-    const tdCreatedAt = createElement('td', formatDate(created_at) );
-    const tdStatus = createElement('td');
-    const tdActions = createElement('td');
-
-    const select = createSelect(status);
-    select.addEventListener('change', ({ target }) => updateTask({ ...task, status:target.value }))
-
-    const editButton = createElement('button', '', '<span class="material-symbols-outlined">edit</span>');
-    const deleteButton = createElement('button', '', '<span class="material-symbols-outlined">delete</span>');
-
-    const editForm = createElement('form');
-    const editInput = createElement('input')
-
-    editForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        
-        updateTask({ id, title: editInput.value, status});
-    }) ;
-
-    editInput.value = title
-    editForm.appendChild(editInput)
-
-    editButton.addEventListener('click', () => {
-        tdTitle.innerText = ''
-        tdTitle.appendChild(editForm)
-    })
-
-    editButton.classList.add('btn-action');
-    deleteButton.classList.add('btn-action');
-
-    deleteButton.addEventListener('click', () => deleteTask(id))
-
-    tdStatus.appendChild(select)
-
-    tdActions.appendChild(editButton);
-    tdActions.appendChild(deleteButton);
-
-    tr.appendChild(tdTitle);
-    tr.appendChild(tdCreatedAt);
-    tr.appendChild(tdStatus);
-    tr.appendChild(tdActions);
-
-    return tr;
-};
-
-const loadTasks = async () => {
-    const tasks = await fetchTasks();
-
-    tbody.innerHTML = '';
-    tasks.forEach((task) => {
-        const tr = createRow(task);
-        tbody.appendChild(tr);
-    });
-};
-
-
-addForm.addEventListener('submit', addTask);
-
-loadTasks();
