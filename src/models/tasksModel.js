47dc0a39da7fdd2952ab2232a8228384bd8d4227
Elias Souza <55217266+liliassz@users.noreply.github.com>
2023-07-21 17:57:27 -0300

    Atualização no codgo

--- src/models/tasksModel.js
+++ src/models/tasksModel.js
@@ -1,33 +1,55 @@
+// Importa o módulo de conexão ao banco de dados
 const connection = require('./connection');
 
+// Função assíncrona que obtém todas as tarefas do banco de dados
 const getAll = async () => {
-    const [tasks] = await connection.execute('SELECT * FROM tasks')
+    // Executa a consulta SQL para selecionar todas as tarefas na tabela 'tasks'
+    const [tasks] = await connection.execute('SELECT * FROM tasks');
+    // Retorna o resultado da consulta, contendo todas as tarefas
     return tasks;
 };
 
-const createTask = async (tasks) => {
-    const { title } = tasks;
+// Função assíncrona que cria uma nova tarefa no banco de dados
+const createTask = async (task) => {
+    // Extrai o campo 'title' do objeto task recebido como parâmetro
+    const { title } = task;
 
-    const dateUTC = new Date(Date.now()).toUTCString()
+    // Obtém a data atual em formato UTC
+    const dateUTC = new Date(Date.now()).toUTCString();
 
+    // Query SQL para inserir uma nova tarefa na tabela 'tasks' com o status 'pendente' e a data de criação
     const query = 'INSERT INTO tasks (title, status, created_at) VALUES (?, ?, ?)';
-    const [createTask] = await connection.execute(query, [title, 'pendente', dateUTC]);
+    // Executa a query, inserindo a nova tarefa no banco de dados
+    const [createTaskResult] = await connection.execute(query, [title, 'pendente', dateUTC]);
 
-    return {insertId: createTask.insertId};
-}
-
-const deleteTask = async (id) => {
-    const removedTask = await connection.execute('DELETE FROM tasks WHERE id = ?', [id])
-    return removedTask
-}
-
-const updateTask = async (id, task) => {
-    const { title, status } = task;
-    const query = 'UPDATE tasks SET title = ?, status = ? WHERE id = ?'
-    const updateTaskTask = await connection.execute(query, [title, status, id]);
-    return updateTask
+    // Retorna o ID da tarefa recém-criada
+    return { insertId: createTaskResult.insertId };
 };
 
+// Função assíncrona que remove uma tarefa do banco de dados com base no seu ID
+const deleteTask = async (id) => {
+    // Executa a consulta SQL para excluir a tarefa com o ID fornecido
+    const removedTask = await connection.execute('DELETE FROM tasks WHERE id = ?', [id]);
+
+    // Retorna o resultado da operação de exclusão da tarefa
+    return removedTask;
+}; 
+
+// Função assíncrona que atualiza uma tarefa existente no banco de dados
+const updateTask = async (id, task) => {
+    // Extrai os campos 'title' e 'status' do objeto task recebido como parâmetro
+    const { title, status } = task;
+
+    // Query SQL para atualizar a tarefa com os novos valores de título e status com base no ID fornecido
+    const query = 'UPDATE tasks SET title = ?, status = ? WHERE id = ?';
+    // Executa a query, atualizando a tarefa no banco de dados
+    const [updateTaskResult] = await connection.execute(query, [title, status, id]);
+
+    // Retorna o resultado da operação de atualização da tarefa
+    return updateTaskResult;
+};
+
+// Exporta as funções do módulo para serem utilizadas em outros módulos do projeto
 module.exports = {
     getAll,
     createTask,
--- src/server.js
+++ src/server.js
@@ -1,5 +1,18 @@
-const app = require('./app');
-require('dotenv').config();
-const PORT = process.env.PORT;
+// Importa o pacote express para criar o aplicativo do servidor
+const express = require('express');
 
-app.listen(PORT, () => console.log(`Servidor ligado na porta: ${PORT}`))
+// Importa o aplicativo de roteamento (app) definido no arquivo app.js
+const app = require('./app');
+
+// Importa o pacote dotenv para carregar variáveis de ambiente a partir de um arquivo .env
+require('dotenv').config();
+
+// Obtém a porta do servidor a partir das variáveis de ambiente
+const PORT = process.env.PORT;
+ 
+// Define a pasta 'public' como a pasta de arquivos estáticos usando o middleware express.static.
+// Isso permite que o servidor sirva os arquivos estáticos (como index.html, styles.css, etc.) localizados na pasta 'public' para o cliente.
+app.use(express.static('public'));
+
+// Inicia o servidor express para escutar na porta especificada
+app.listen(PORT, () => console.log(`Servidor ligado na porta: ${PORT}`));
